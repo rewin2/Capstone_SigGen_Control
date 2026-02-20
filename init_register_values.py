7ba8800663293685769680347e04280300926c09
Reece Winmond <rewin2@morgan.edu>
2026-02-19 21:52:47 -0500

    Changed to 16-bit values, addresses removed

--- init_register_values.py
+++ init_register_values.py
@@ -2,12 +2,12 @@ from register_map import *
 
 # Static power-up values (datasheet / TI tool derived)
 INIT_REG_VALUES = {
-    SYS_CTRL_REG:     0x006471,
-    SYS_PWR_REG:      0x0157A0,
-    SYS_RESET_REG:    0x0281F4,
-    REF_CTRL_REG:     0x0A0000,
-    REF_DIV_REG:      0x0B0612,
-    VCO_BIAS_REG:     0x320080,
-    VCO_GAIN_REG:     0x33203F,
-    RFOUTA_EN_REG:    0x470000,
+    SYS_CTRL_REG:     0x6471,
+    SYS_PWR_REG:      0x57A0,
+    SYS_RESET_REG:    0x81F4,
+    REF_CTRL_REG:     0x0000,
+    REF_DIV_REG:      0x0612,
+    VCO_BIAS_REG:     0x0080,
+    VCO_GAIN_REG:     0x203F,
+    RFOUTA_EN_REG:    0x0000,
 }
